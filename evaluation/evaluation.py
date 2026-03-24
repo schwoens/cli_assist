53115d293e048b68600057e3856af2d641ea1e60
schwoens <schwoens@protonmail.com>
2026-03-25 00:26:37 +0100

    finalize evaluation script

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -8,14 +8,23 @@ partialCorrection = 0
 error = 0
 skipped = 0
 
-charSwapSuccess = 0
-missingCharSuccess = 0
-extraCharSuccess = 0
-nearbyCharSuccess = 0
-skippedSpaceSuccess = 0
-randomSpaceSuccess = 0
-repeatedCharSuccess = 0
-unicharSuccess = 0
+charSwapSuccesses = 0
+missingCharSuccesses = 0
+extraCharSuccesses = 0
+nearbyCharSuccesses = 0
+skippedSpaceSuccesses = 0
+randomSpaceSuccesses = 0
+repeatedCharSuccesses = 0
+unicharSuccesses = 0
+
+charSwapPartialSuccesses = 0
+missingCharPartialSuccesses = 0
+extraCharPartialSuccesses = 0
+nearbyCharPartialSuccesses = 0
+skippedSpacePartialSuccesses = 0
+randomSpacePartialSuccesses = 0
+repeatedCharPartialSuccesses = 0
+unicharPartialSuccesses = 0
 
 charSwapSkips = 0
 missingCharSkips = 0
@@ -46,18 +55,18 @@ def run(input, expected):
             return ('no correction', correction)
         else:
             partialCorrection += 1;
-            return ('partial correction', correction)
+            return ('partial success', correction)
     else:
         error += 1
         return ('error', result.stderr)
 
-def getColor(result):
+def get_color(result):
     match result:
         case 'success':
             return 'green'
         case 'no correction':
             return 'yellow'
-        case 'partial correction':
+        case 'partial success':
             return 'magenta'
         case 'error':
             return 'red'
@@ -65,14 +74,24 @@ def getColor(result):
             return 'light_grey'
 
 def test(line):
-    global charSwapSuccess
-    global missingCharSuccess
-    global extraCharSuccess
-    global nearbyCharSuccess
-    global skippedSpaceSuccess
-    global randomSpaceSuccess
-    global repeatedCharSuccess
-    global unicharSuccess
+    global charSwapSuccesses
+    global missingCharSuccesses
+    global extraCharSuccesses
+    global nearbyCharSuccesses
+    global skippedSpaceSuccesses
+    global randomSpaceSuccesses
+    global repeatedCharSuccesses
+    global unicharSuccesses
+
+    global charSwapPartialSuccesses
+    global missingCharPartialSuccesses
+    global extraCharPartialSuccesses
+    global nearbyCharPartialSuccesses
+    global skippedSpacePartialSuccesses
+    global randomSpacePartialSuccesses
+    global repeatedCharPartialSuccesses
+    global unicharPartialSuccesses
+
     global charSwapSkips
     global missingCharSkips
     global extraCharSkips
@@ -82,94 +101,102 @@ def test(line):
     global repeatedCharSkips
     global unicharSkips
 
-    print(f'Testing: "{line}" ... length: {len(line)}\n')
+    print(f'{line}; length: {len(line)}\n')
     charSwapErrorInput = typo.StrErrer(line).char_swap().result
     result = run(charSwapErrorInput, line)
-    color = getColor(result[0])
-    cprint(f'Character swap: {charSwapErrorInput} ... {result[0]}', color)
-    if result[0] == 'success':
-        charSwapSuccess += 1
-    elif result[0] == 'skipped':
-        charSwapSkips += 1
-    else:
-        cprint(f'\tgot: {result[1]}', color)
+    color = get_color(result[0])
+    cprint(f'character swap: {charSwapErrorInput} ... {result}', color)
+    match result[0]:
+        case 'success':
+            charSwapSuccesses += 1
+        case 'skipped':
+            charSwapSkips += 1
+        case 'partial success':
+            charSwapPartialSuccesses += 1
 
     missingCharErrorInput = typo.StrErrer(line).missing_char().result
     result = run(missingCharErrorInput, line)
-    color = getColor(result[0]) 
-    cprint(f'Missing character: {missingCharErrorInput} ... {result[0]}', color)
-    if result[0] == 'success':
-        missingCharSuccess += 1
-    elif result[0] == 'skipped':
-        missingCharSkips += 1
-    else:
-        cprint(f'\tgot: {result[1]}', color)
+    color = get_color(result[0]) 
+    cprint(f'missing character: {missingCharErrorInput} ... {result}', color)
+    match result[0]:
+        case 'success':
+            missingCharSuccesses += 1
+        case 'skipped':
+            missingCharSkips += 1
+        case 'partial success':
+            missingCharPartialSuccesses += 1
 
     extraCharErrorInput = typo.StrErrer(line).extra_char().result
     result = run(extraCharErrorInput, line)
-    color = getColor(result[0]) 
-    cprint(f'Extra character: {extraCharErrorInput} ... {result[0]}', color)
-    if result[0] == 'success':
-        extraCharSuccess += 1
-    elif result[0] == 'skipped':
-        extraCharSkips += 1
-    else:
-        cprint(f'\tgot: {result[1]}', color)
+    color = get_color(result[0]) 
+    cprint(f'extra character: {extraCharErrorInput} ... {result}', color)
+    match result[0]:
+        case 'success':
+            extraCharSuccesses += 1
+        case 'skipped':
+            extraCharSkips += 1
+        case 'partial success':
+            extraCharPartialSuccesses += 1
 
     nearbyCharErrorInput = typo.StrErrer(line).nearby_char().result
     result = run(nearbyCharErrorInput, line)
-    color = getColor(result[0])
-    cprint(f'Nearby character: {nearbyCharErrorInput} ... {result[0]}', color)
-    if result[0] =='success':
-        nearbyCharSuccess += 1
-    elif result[0] == 'skipped':
-        nearbyCharSkips += 1
-    else:
-        cprint(f'\tgot: {result[1]}', color)
+    color = get_color(result[0])
+    cprint(f'nearby character: {nearbyCharErrorInput} ... {result}', color)
+    match result[0]:
+        case 'success':
+            nearbyCharSuccesses += 1
+        case 'skipped':
+            nearbyCharSkips += 1
+        case 'partial success':
+            nearbyCharPartialSuccesses += 1
 
     skippedSpaceErrorInput = typo.StrErrer(line).skipped_space().result
     result = run(skippedSpaceErrorInput, line)
-    color = getColor(result[0])
-    cprint(f'Skipped space: {skippedSpaceErrorInput} ... {result[0]}', color)
-    if result[0] == 'success':
-        skippedSpaceSuccess += 1
-    elif result[0] == 'skipped':
-        skippedSpaceSkips += 1
-    else:
-        cprint(f'\tgot: {result[1]}', color)
+    color = get_color(result[0])
+    cprint(f'skipped space: {skippedSpaceErrorInput} ... {result}', color)
+    match result[0]:
+        case 'success':
+            skippedSpaceSuccesses += 1
+        case 'skipped':
+            skippedSpaceSkips += 1
+        case 'partial success':
+            skippedSpacePartialSuccesses += 1
 
     randomSpaceErrorInput = typo.StrErrer(line).random_space().result
     result = run(randomSpaceErrorInput, line)
-    color = getColor(result[0]) 
-    cprint(f'Random space: {randomSpaceErrorInput} ... {result[0]}', color)
-    if result[0] == 'success':
-        randomSpaceSuccess += 1
-    elif result[0] == 'skipped':
-        randomSpaceSkips += 1
-    else:
-        cprint(f'\tgot: {result[1]}', color)
+    color = get_color(result[0]) 
+    cprint(f'random space: {randomSpaceErrorInput} ... {result}', color)
+    match result[0]:
+        case 'success':
+            randomSpaceSuccesses += 1
+        case 'skipped':
+            randomSpaceSkips += 1
+        case 'partial success':
+            randomSpacePartialSuccesses += 1
 
     repeatedCharErrorInput = typo.StrErrer(line).repeated_char().result
     result = run(repeatedCharErrorInput, line)
-    color = getColor(result[0]) 
-    cprint(f'Repeated character: {repeatedCharErrorInput} ... {result[0]}', color)
-    if result[0] == 'success':
-        repeatedCharSuccess += 1
-    elif result[0] == 'skipped':
-        repeatedCharSkips += 1
-    else:
-        cprint(f'\tgot: {result[1]}', color)
+    color = get_color(result[0]) 
+    cprint(f'repeated character: {repeatedCharErrorInput} ... {result}', color)
+    match result[0]:
+        case 'success':
+            repeatedCharSuccesses += 1
+        case 'skipped':
+            repeatedCharSkips += 1
+        case 'partial success':
+            repeatedCharPartialSuccesses += 1
 
     unicharErrorInput = typo.StrErrer(line).unichar().result
     result = run(unicharErrorInput, line)
-    color = getColor(result[0])
-    cprint(f'Unicharacter: {unicharErrorInput} ... {result[0]}', color)
-    if result[0] == 'success':
-        unicharSuccess += 1
-    elif result[0] == 'skipped':
-        unicharSkips += 1
-    else:
-        cprint(f'\tgot: {result[1]}', color)
+    color = get_color(result[0])
+    cprint(f'unicharacter: {unicharErrorInput} ... {result}', color)
+    match result[0]:
+        case 'success':
+            unicharSuccesses += 1
+        case 'skipped':
+            unicharSkips += 1
+        case 'partial success':
+            unicharPartialSuccesses += 1
 
     print()
 
@@ -283,21 +310,21 @@ for commandLine in testCommandLines:
 
 print('===========================================================\n')
 
-print(f'Character swap: {charSwapSuccess}/{len(testCommandLines)-charSwapSkips}')
-print(f'Missing character: {missingCharSuccess}/{len(testCommandLines)-missingCharSkips}')
-print(f'Extra character: {extraCharSuccess}/{len(testCommandLines)-extraCharSkips}')
-print(f'Nearby character: {nearbyCharSuccess}/{len(testCommandLines)-nearbyCharSkips}')
-print(f'Skipped space: {skippedSpaceSuccess}/{len(testCommandLines)-skippedSpaceSkips}')
-print(f'Random space: {randomSpaceSuccess}/{len(testCommandLines)-randomSpaceSkips}')
-print(f'Repeated character: {repeatedCharSuccess}/{len(testCommandLines)-repeatedCharSkips}')
-print(f'Unicharacter: {unicharSuccess}/{len(testCommandLines)-unicharSkips}')
+print(f'Character swap: {charSwapSuccesses}/{charSwapPartialSuccesses}/{len(testCommandLines)-charSwapSkips}')
+print(f'Missing character: {missingCharSuccesses}/{missingCharPartialSuccesses}/{len(testCommandLines)-missingCharSkips}')
+print(f'Extra character: {extraCharSuccesses}/{extraCharPartialSuccesses}/{len(testCommandLines)-extraCharSkips}')
+print(f'Nearby character: {nearbyCharSuccesses}/{nearbyCharPartialSuccesses}/{len(testCommandLines)-nearbyCharSkips}')
+print(f'Skipped space: {skippedSpaceSuccesses}/{skippedSpacePartialSuccesses}/{len(testCommandLines)-skippedSpaceSkips}')
+print(f'Random space: {randomSpaceSuccesses}/{randomSpacePartialSuccesses}/{len(testCommandLines)-randomSpaceSkips}')
+print(f'Repeated character: {repeatedCharSuccesses}/{repeatedCharPartialSuccesses}/{len(testCommandLines)-repeatedCharSkips}')
+print(f'Unicharacter: {unicharSuccesses}/{unicharPartialSuccesses}/{len(testCommandLines)-unicharSkips}')
 
 print('\n===========================================================\n')
 
 print('TOTAL')
 print(f'Successes: {success}')
-print(f'Partial corrections: {partialCorrection}')
-print(f'Missing corrections: {noCorrection}')
+print(f'Partial successes: {partialCorrection}')
+print(f'No corrections: {noCorrection}')
 print(f'Skips: {skipped}')
 print(f'Errors: {error}')
 
